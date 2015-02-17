1ab212f5188d17f02cec292db31885f02dcdcb5e
smolcoder <smolcoder@gmail.com>
2015-02-18 00:52:52 +0300

    redirect from main page to searcher

--- genequery/urls.py
+++ genequery/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.core.urlresolvers import reverse_lazy, reverse
 from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
-                       url(r'^$', 'searcher.views.search_page_view', name='index'),
+                       url(r'^$', RedirectView.as_view(url=reverse_lazy('searcher:index')), name='index'),
                        url(r'^about/$', 'main.views.about_page_view', name='about'),
                        url(r'^contacts/$', 'main.views.contacts_page_view', name='contacts'),
                        url(r'^example/$', 'main.views.example_page_view', name='example'),
